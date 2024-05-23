392b2d7191a1c5caa76f955da0426ca9b301b0a8
rishavkumar7 <rishavkumarofficial7@gmail.com>
2024-05-23 15:58:09 +0530

    episode-5 after hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import restaurantData from "./../utils/mockData";
 
 const Body = () => {
+    const arr = useState(restaurantData);
+
     return (
         <div className="body">
-            <div className="searchBar">
-                Search
+            <div className="filterButton">
+                <button onClick={() => {arr[1](arr[0].filter(res => res.card.card.info.avgRating >= 4.0))}}>Top Rated Restaurants</button>
             </div>
             <div className="restaurantContainer">
-                { restaurantData.map(restaurant => <RestaurantCard key={restaurant.card.card.info.id} resData={restaurant} />) }
+                { arr[0].map(restaurant => <RestaurantCard key={restaurant.card.card.info.id} resData={restaurant} />) }
             </div>
         </div>
     );
